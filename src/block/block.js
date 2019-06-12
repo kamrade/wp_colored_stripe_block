396ac1c2248a5ccae73219c605cd90368263db7a
Dennis Mikhailov <denis.michailov@gmail.com>
2019-06-12 21:51:29 +1000

    Practice

--- src/block/block.js
+++ src/block/block.js
@@ -26,68 +26,93 @@ const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.b
  *                             registered; otherwise `undefined`.
  */
 registerBlockType( 'cgb/block-colored-stripe-block', {
-	// Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
-	title: __( 'colored-stripe-block - CGB Block' ), // Block title.
-	icon: 'shield', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
-	category: 'common', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
-	keywords: [
-		__( 'colored-stripe-block — CGB Block' ),
-		__( 'CGB Example' ),
-		__( 'create-guten-block' ),
-	],
+  // Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
+  title: __( 'colored-stripe-block - CGB Block' ), // Block title.
+  icon: 'shield', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
+  category: 'common', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
+  keywords: [
+    __( 'colored-stripe-block — CGB Block' ),
+    __( 'CGB Example' ),
+    __( 'create-guten-block' ),
+  ],
+  attributes: {
+    block01: {
+      type:   'string',
+    },
+    block02: {
+      type:   'string'
+    },
+  },
 
-	/**
-	 * The edit function describes the structure of your block in the context of the editor.
-	 * This represents what the editor will render when the block is used.
-	 *
-	 * The "edit" property must be a valid function.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-	 */
-	edit: function( props ) {
-		// Creates a <p class='wp-block-cgb-block-colored-stripe-block'></p>.
-		return (
-			<div className={ props.className }>
-				<p>— Hello from the backend.</p>
-				<p>
-					CGB BLOCK: <code>colored-stripe-block</code> is a new Gutenberg block
-				</p>
-				<p>
-					It was created via{ ' ' }
-					<code>
-						<a href="https://github.com/ahmadawais/create-guten-block">
-							create-guten-block
-						</a>
-					</code>.
-				</p>
-			</div>
-		);
-	},
+  /**
+   * The edit function describes the structure of your block in the context of the editor.
+   * This represents what the editor will render when the block is used.
+   *
+   * The "edit" property must be a valid function.
+   *
+   * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
+   */
+  edit: function( props ) {
 
-	/**
-	 * The save function defines the way in which the different attributes should be combined
-	 * into the final markup, which is then serialized by Gutenberg into post_content.
-	 *
-	 * The "save" property must be specified and must be a valid function.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-	 */
-	save: function( props ) {
-		return (
-			<div>
-				<p>— Hello from the frontend.</p>
-				<p>
-					CGB BLOCK: <code>colored-stripe-block</code> is a new Gutenberg block.
-				</p>
-				<p>
-					It was created via{ ' ' }
-					<code>
-						<a href="https://github.com/ahmadawais/create-guten-block">
-							create-guten-block
-						</a>
-					</code>.
-				</p>
-			</div>
-		);
-	},
+    function updateContent(event, block) {
+      props.setAttributes({ [block]: event.target.value });
+    }
+
+    // Creates a <p class='wp-block-cgb-block-colored-stripe-block'></p>.
+    return (
+      <div className={ props.className }>
+
+      <div className="form-group">
+        <label className="d-block">
+          Content:
+          <input
+            className="form-control"
+            value={props.attributes.block01}
+            onChange={() => updateContent(event, 'block01') }
+            type="text" />
+        </label>
+      </div>
+
+      <div className="form-group">
+        <label className="d-block">
+          Color:
+          <input
+            className="form-control"
+            value={props.attributes.block02}
+            onChange={() => updateContent(event, 'block02') }
+            type="text" />
+        </label>
+      </div>
+
+      </div>
+    );
+  },
+
+  /**
+   * The save function defines the way in which the different attributes should be combined
+   * into the final markup, which is then serialized by Gutenberg into post_content.
+   *
+   * The "save" property must be specified and must be a valid function.
+   *
+   * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
+   */
+  save: function( props ) {
+
+    return (
+      <div>
+        <p>— Hello from the frontend.</p>
+        <p>
+          CGB BLOCK: <code>colored-stripe-block</code> is a new Gutenberg block.
+        </p>
+        <p>
+          It was created via{ ' ' }
+          <code>
+            <a href="https://github.com/ahmadawais/create-guten-block">
+              create-guten-block
+            </a>
+          </code>.
+        </p>
+      </div>
+    );
+  },
 } );
--- src/init.php
+++ src/init.php
@@ -10,7 +10,7 @@
 
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+  exit;
 }
 
 /**
@@ -28,51 +28,51 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @since 1.0.0
  */
 function colored_stripe_block_cgb_block_assets() { // phpcs:ignore
-	// Register block styles for both frontend + backend.
-	wp_register_style(
-		'colored_stripe_block-cgb-style-css', // Handle.
-		plugins_url( 'dist/blocks.style.build.css', dirname( __FILE__ ) ), // Block style CSS.
-		array( 'wp-editor' ), // Dependency to include the CSS after it.
-		null // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.style.build.css' ) // Version: File modification time.
-	);
+  // Register block styles for both frontend + backend.
+  wp_register_style(
+    'colored_stripe_block-cgb-style-css', // Handle.
+    plugins_url( 'dist/blocks.style.build.css', dirname( __FILE__ ) ), // Block style CSS.
+    array( 'wp-editor' ), // Dependency to include the CSS after it.
+    null // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.style.build.css' ) // Version: File modification time.
+  );
 
-	// Register block editor script for backend.
-	wp_register_script(
-		'colored_stripe_block-cgb-block-js', // Handle.
-		plugins_url( '/dist/blocks.build.js', dirname( __FILE__ ) ), // Block.build.js: We register the block here. Built with Webpack.
-		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor' ), // Dependencies, defined above.
-		null, // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.build.js' ), // Version: filemtime — Gets file modification time.
-		true // Enqueue the script in the footer.
-	);
+  // Register block editor script for backend.
+  wp_register_script(
+    'colored_stripe_block-cgb-block-js', // Handle.
+    plugins_url( '/dist/blocks.build.js', dirname( __FILE__ ) ), // Block.build.js: We register the block here. Built with Webpack.
+    array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor' ), // Dependencies, defined above.
+    null, // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.build.js' ), // Version: filemtime — Gets file modification time.
+    true // Enqueue the script in the footer.
+  );
 
-	// Register block editor styles for backend.
-	wp_register_style(
-		'colored_stripe_block-cgb-block-editor-css', // Handle.
-		plugins_url( 'dist/blocks.editor.build.css', dirname( __FILE__ ) ), // Block editor CSS.
-		array( 'wp-edit-blocks' ), // Dependency to include the CSS after it.
-		null // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.editor.build.css' ) // Version: File modification time.
-	);
+  // Register block editor styles for backend.
+  wp_register_style(
+    'colored_stripe_block-cgb-block-editor-css', // Handle.
+    plugins_url( 'dist/blocks.editor.build.css', dirname( __FILE__ ) ), // Block editor CSS.
+    array( 'wp-edit-blocks' ), // Dependency to include the CSS after it.
+    null // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.editor.build.css' ) // Version: File modification time.
+  );
 
-	/**
-	 * Register Gutenberg block on server-side.
-	 *
-	 * Register the block on server-side to ensure that the block
-	 * scripts and styles for both frontend and backend are
-	 * enqueued when the editor loads.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/blocks/writing-your-first-block-type#enqueuing-block-scripts
-	 * @since 1.16.0
-	 */
-	register_block_type(
-		'cgb/block-colored-stripe-block', array(
-			// Enqueue blocks.style.build.css on both frontend & backend.
-			'style'         => 'colored_stripe_block-cgb-style-css',
-			// Enqueue blocks.build.js in the editor only.
-			'editor_script' => 'colored_stripe_block-cgb-block-js',
-			// Enqueue blocks.editor.build.css in the editor only.
-			'editor_style'  => 'colored_stripe_block-cgb-block-editor-css',
-		)
-	);
+  /**
+   * Register Gutenberg block on server-side.
+   *
+   * Register the block on server-side to ensure that the block
+   * scripts and styles for both frontend and backend are
+   * enqueued when the editor loads.
+   *
+   * @link https://wordpress.org/gutenberg/handbook/blocks/writing-your-first-block-type#enqueuing-block-scripts
+   * @since 1.16.0
+   */
+  register_block_type(
+    'cgb/block-colored-stripe-block', array(
+      // Enqueue blocks.style.build.css on both frontend & backend.
+      'style'         => 'colored_stripe_block-cgb-style-css',
+      // Enqueue blocks.build.js in the editor only.
+      'editor_script' => 'colored_stripe_block-cgb-block-js',
+      // Enqueue blocks.editor.build.css in the editor only.
+      'editor_style'  => 'colored_stripe_block-cgb-block-editor-css',
+    )
+  );
 }
 
 // Hook: Block assets.
